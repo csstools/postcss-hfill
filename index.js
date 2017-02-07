531bdfbb0b404ebd608eff70bb4dbbbc5755f4e1
Jonathan Neal <jonathantneal@hotmail.com>
2017-02-06 23:48:23 -0500

    Remove generatorics dependency

--- index.js
+++ index.js
@@ -1,50 +1,68 @@
 // tooling
-const generatorics = require('generatorics');
-const postcss      = require('postcss');
+const postcss = require('postcss');
 
 // sectioning elements
 const sections = ['article', 'aside', 'nav', 'section'];
+const length   = sections.length;
 
 // plugin
 module.exports = postcss.plugin('postcss-hfill', ({
 	sizes       = ['2em', '1.5em', '1.17em', '1em'],
 	tag         = 'x-h'
-}) => (root) => {
+} = {}) => (root) => {
+	// generate contextual heading font-sizes using only element specificity
+	const cssRules = sizes.reduce(
+		(rules, value, size) => {
+			const total = Math.pow(length, size);
+			const result = [];
+
+			for (let i = -1; ++i < total;) {
+				const combo = [];
+
+				for (let ii = i, j = -1; ++j < size;) {
+					let modulus = ii % length;
+
+					combo.push(sections[modulus]);
+
+					ii = (ii - modulus) / length;
+				}
+
+				result.push(combo);
+			}
+
+			return rules.concat(
+				postcss.rule({
+					selectors: result.map(
+						(x) => x.length ? x.join(' ').concat(` ${ tag }`) : tag
+					),
+					nodes: [
+						postcss.decl({
+							prop: 'font-size',
+							value: sizes[size],
+							source: source,
+							raws: declRaws
+						})
+					],
+					source: source,
+					raws: ruleRaws
+				})
+			);
+		},
+		[]
+	);
+
 	// prepend contextual heading font-sizes using only element specificity
 	root.prepend(
-		permutatedSections(tag, sizes)
+		cssRules
 	);
 });
 
-// generate contextual heading font-sizes using only element specificity
-function permutatedSections(tag, sizes) {
-	const rules = [];
-
-	for (let depth = 0; sizes[depth]; ++depth) {
-		rules.push(
-			postcss.rule({
-				// selector is all permutations of sections by depth
-				selector: Array.from(
-					generatorics.clone.baseN(sections, depth)
-				).map(
-					(x) => x.length ? x.join(' ').concat(` ${ tag }`) : tag
-				).join(','),
-				// declaration is font-size of depth
-				nodes: [
-					postcss.decl({
-						prop: 'font-size',
-						value: sizes[depth],
-						raws: declRaws
-					})
-				],
-				raws: ruleRaws
-			})
-		);
-	}
-
-	return rules;
-}
 
 // minified raws
 const declRaws = { before: '', between: ':', after: '' };
 const ruleRaws = { before: '\n', between: '', after: '', semicolon: false };
+const source = {
+	input: {
+		file: 'postcss-hfill'
+	}
+};
